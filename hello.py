8213fd68b2fa076c6b1a9bbf3d6e5525f5fa57fa
TAMILSELVAN <selvatamil2095@gmail.com>
2024-11-05 20:00:00 +0530

    this is first commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+package org.company;
+
+public class CompanyInfo {
+	public static void main(String[] args) {
+		CompanyInfo objCom=new CompanyInfo();
+		objCom.companyName();
+		objCom.companyId();
+		objCom.companyAddress();
+	}
+public void companyName() {
+	System.out.println("company Name ");
+}
+public void companyId() {
+	System.out.println("company Id ");
+}
+public void companyAddress() {
+	System.out.println("company Address ");
+}
+}
+
